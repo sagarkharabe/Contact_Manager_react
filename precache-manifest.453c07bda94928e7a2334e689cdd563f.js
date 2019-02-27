dee126d05ae3e572b61a21ce3f6a60730f5c6454
RaghunathVadlakonda <sagarkharabe97@gmail.com>
2019-02-27 10:11:21 +0530

    Updates

---
+++ precache-manifest.453c07bda94928e7a2334e689cdd563f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa0ca202027b969dcd2e",
+    "url": "/Contact_Manager_react/static/js/runtime~main.fa0ca202.js"
+  },
+  {
+    "revision": "14a83280c423e713815d",
+    "url": "/Contact_Manager_react/static/js/main.14a83280.chunk.js"
+  },
+  {
+    "revision": "fec3f85401e20f3a9f78",
+    "url": "/Contact_Manager_react/static/js/1.fec3f854.chunk.js"
+  },
+  {
+    "revision": "14a83280c423e713815d",
+    "url": "/Contact_Manager_react/static/css/main.77f23a85.chunk.css"
+  },
+  {
+    "revision": "fec3f85401e20f3a9f78",
+    "url": "/Contact_Manager_react/static/css/1.6f4237de.chunk.css"
+  },
+  {
+    "revision": "4e80112093afb30fb5449aed7151dc21",
+    "url": "/Contact_Manager_react/index.html"
+  }
+];
